7599e136e783bc044e5eb400520726c6ae2d5f85
SDDAAMM <45061050+SDDAAMM@users.noreply.github.com>
2021-01-26 09:15:39 +0100

    cambio4

--- prueba1/src/paquetePrueba1/Clase1.java
+++ prueba1/src/paquetePrueba1/Clase1.java
@@ -4,6 +4,7 @@ public class Clase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int numero1;
 		System.out.println("Hola mundo");
 		System.out.println("Hola que tal, soy colosal");
 		System.out.println("Adios mundo");
